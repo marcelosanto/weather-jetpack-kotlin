fd56bfd57934384eb33c1dbbe98a9eeab8bb9b6a
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-05 23:04:44 -0300

    criado o WeatherSplashScreen

--- app/src/main/java/xyz/spacer/weatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/xyz/spacer/weatherforecast/screens/WeatherSplashScreen.kt
@@ -1,10 +1,50 @@
 package xyz.spacer.weatherforecast.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import xyz.spacer.weatherforecast.R
 
 @Composable
 fun WeatherSplashScreen(navController: NavHostController) {
-    Text(text = "Splash Screen")
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(2.dp, Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun), contentDescription = "Sun icon",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(95.dp)
+            )
+            Text(
+                text = "Find the Sun?",
+                style = MaterialTheme.typography.h5,
+                color = Color.LightGray
+            )
+        }
+    }
 }
