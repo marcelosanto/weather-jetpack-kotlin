474a9124e9216a73b219130d71777b634fa3f3ab
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-09 16:41:59 -0300

    terminado o efeito animado na WeatherSplashScreen

--- app/src/main/java/xyz/spacer/weatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/xyz/spacer/weatherforecast/screens/WeatherSplashScreen.kt
@@ -1,5 +1,8 @@
 package xyz.spacer.weatherforecast.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -11,21 +14,46 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
 import xyz.spacer.weatherforecast.R
+import xyz.spacer.weatherforecast.navigation.WeatherScreens
 
 @Composable
 fun WeatherSplashScreen(navController: NavHostController) {
+
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(targetValue = 0.9f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(8f)
+                        .getInterpolation(it)
+                }
+            ))
+
+        delay(2000L)
+        navController.navigate(WeatherScreens.MainScreen.name)
+    })
+
     Surface(
         modifier = Modifier
             .padding(15.dp)
-            .size(330.dp),
+            .size(330.dp)
+            .scale(scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(2.dp, Color.LightGray)
