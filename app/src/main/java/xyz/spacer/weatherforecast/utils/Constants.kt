36c1d8aa03c8804a792fc1ea2c494d12afa224d4
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-11 15:04:29 -0300

    Criado interface weatherRepository, DataOrException e AppModule

--- app/src/main/java/xyz/spacer/weatherforecast/utils/Constants.kt
+++ app/src/main/java/xyz/spacer/weatherforecast/utils/Constants.kt
@@ -2,5 +2,5 @@ package xyz.spacer.weatherforecast.utils
 
 object Constants {
     const val BASE_URL = "https://api.hgbrasil.com/"
-    const val API_KEY = System.getenv("API_KEY")
+    const val API_KEY = "sadadsad"
 }
