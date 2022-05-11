c413d1453a624f64acdd774bd9ef5cbd93b0d07d
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-11 14:53:40 -0300

    Criado interface weatherRepository, DataOrException e AppModule

--- app/src/main/java/xyz/spacer/weatherforecast/data/DataOrException.kt
+++
@@ -1,7 +0,0 @@
-package xyz.spacer.weatherforecast.data
-
-class DataOrException<T, Boolean, E : Exception>(
-    var data: T? = null,
-    var loading: kotlin.Boolean? = null,
-    var e: E? = null
-) {}
