d9367204c2c8e17f614c1ce2e267b75d07d6f1b4
Marcelo Santos <rocha.mer21@gmail.com>
2022-05-11 20:55:45 -0300

    Criado interface weatherRepository, DataOrException e AppModule

--- app/src/main/java/xyz/spacer/weatherforecast/di/AppModule.kt
+++ app/src/main/java/xyz/spacer/weatherforecast/di/AppModule.kt
@@ -1,5 +1,25 @@
 package xyz.spacer.weatherforecast.di
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import xyz.spacer.weatherforecast.network.WeatherAPI
+import xyz.spacer.weatherforecast.utils.Constants
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+    @Provides
+    @Singleton
+    fun provideOpenWeatherApi(): WeatherAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherAPI::class.java)
+    }
 }
