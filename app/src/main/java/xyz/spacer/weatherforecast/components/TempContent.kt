f77a558c5d39e9f1531c04472ca00071a3b5b83f
Marcelo Santos <rocha.mer21@gmail.com>
2022-08-16 15:48:45 -0300

    TempContent e todayInfo componentes criados

---
+++ app/src/main/java/xyz/spacer/weatherforecast/components/TempContent.kt
@@ -0,0 +1,106 @@
+package xyz.spacer.weatherforecast.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.TabRowDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import xyz.spacer.weatherforecast.R
+import xyz.spacer.weatherforecast.model.Weather
+import java.util.*
+
+@Composable
+fun TempContent(data: Weather) {
+    Column(
+        Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Image(painterResource(id = R.drawable.sunrise), contentDescription = "")
+        Text(
+            text = "${data.results.temp}º",
+            color = Color.DarkGray,
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = data.results.description,
+            fontWeight = FontWeight.SemiBold,
+            color = Color.DarkGray,
+            fontSize = 20.sp
+        )
+        Spacer(Modifier.height(20.0.dp))
+
+        CardTempInfo(data)
+    }
+}
+
+@Composable
+private fun CardTempInfo(data: Weather) {
+    Card(
+        Modifier.padding(10.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(24.dp),
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically,
+
+            ) {
+
+            ColumnInfo("Wind", data.results.wind_speedy, R.drawable.wind)
+            DividerRows()
+            ColumnInfo("Humidity", "${data.results.humidity}%",R.drawable.humidity)
+            DividerRows()
+            ColumnInfo("Currently", data.results.currently.capitalize(Locale.ROOT),R.drawable.sunrise)
+        }
+    }
+}
+
+@Composable
+private fun DividerRows() {
+    TabRowDefaults.Divider(
+        color = Color.Gray,
+        modifier = Modifier
+            .height(30.dp)
+            .width(1.dp)
+            .alpha(0.5f)
+    )
+}
+
+@Composable
+private fun ColumnInfo(title: String, info: String, icon: Int) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+            Image(
+                painterResource(id = icon),
+                contentDescription = "today image",
+                Modifier
+                    .width(20.dp)
+                    .height(20.dp),
+            )
+            Text(text = title, fontSize = 20.sp, color = Color.Gray)
+        }
+
+        Text(text = info, fontSize = 18.sp, color = Color.DarkGray, fontWeight = FontWeight.Bold)
+    }
+}
--- app/src/main/java/xyz/spacer/weatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/xyz/spacer/weatherforecast/screens/main/MainScreen.kt
@@ -1,14 +1,24 @@
 package xyz.spacer.weatherforecast.screens.main
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import xyz.spacer.weatherforecast.R
+import xyz.spacer.weatherforecast.components.TempContent
 import xyz.spacer.weatherforecast.data.DataOrException
 import xyz.spacer.weatherforecast.model.Weather
 import xyz.spacer.weatherforecast.widgets.WeatherAppBar
@@ -39,18 +49,62 @@ fun MainScaffold(weather: Weather, navController: NavHostController) {
         WeatherAppBar(
             title = weather.results.city,
             navController = navController,
-            elevation = 5.dp
         ) {
             Log.d("BUTTON", "MainScaffold: ButtonClicked")
         }
     }
     ) {
-        MainContent(data = weather)
+        Column() {
+            TempContent(data = weather)
+            TodayContent(data = weather)
+        }
+
+    }
+
+}
+
+
+@Composable
+fun TodayContent(data: Weather) {
+    Text(text = "Today", Modifier.padding(start = 20.dp, bottom = 10.dp))
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(start = 20.dp, end = 20.dp, bottom = 0.dp)
+            .background(Color.Red),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        TodayInfo(vertical = Arrangement.Center)
+        TodayInfo(vertical = Arrangement.Top)
+        TodayInfo(vertical = Arrangement.Bottom)
+
+
     }
 
 }
 
 @Composable
-fun MainContent(data: Weather) {
-    Text(text = data.results.city_name)
+private fun TodayInfo(vertical: Arrangement.Vertical) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .width(80.dp)
+            .background(Color.Blue),
+        verticalArrangement = vertical,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Text(text = "12 PM", fontSize = 12.sp)
+        Image(
+            painterResource(id = R.drawable.sun),
+            contentDescription = "today image",
+            Modifier
+                .width(40.dp)
+                .height(40.dp),
+        )
+        Text(text = "18º")
+    }
 }
