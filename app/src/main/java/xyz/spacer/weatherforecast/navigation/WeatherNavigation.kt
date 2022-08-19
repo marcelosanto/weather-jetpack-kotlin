e3c9d16fed6190a6df45b8dc7f7a85ab6ae7bd29
Marcelo Santos <rocha.mer21@gmail.com>
2022-08-19 17:20:35 -0300

    feat: add arguments in router

--- app/src/main/java/xyz/spacer/weatherforecast/navigation/WeatherNavigation.kt
+++ app/src/main/java/xyz/spacer/weatherforecast/navigation/WeatherNavigation.kt
@@ -2,9 +2,11 @@ package xyz.spacer.weatherforecast.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import xyz.spacer.weatherforecast.screens.main.MainScreen
 import xyz.spacer.weatherforecast.screens.main.MainViewModel
 import xyz.spacer.weatherforecast.screens.search.SearchScreen
@@ -22,9 +24,18 @@ fun WeatherNavigation() {
             WeatherSplashScreen(navController = navController)
         }
 
-        composable(WeatherScreens.MainScreen.name) {
-            val mainViewModel = hiltViewModel<MainViewModel>()
-            MainScreen(navController = navController, mainViewModel)
+
+        val route = WeatherScreens.MainScreen.name
+        composable("$route/{city}", arguments = listOf(
+            navArgument(name = "city") {
+                type = NavType.StringType
+            }
+        )) { navBack ->
+            navBack.arguments?.getString("city").let { city ->
+
+                val mainViewModel = hiltViewModel<MainViewModel>()
+                MainScreen(navController = navController, mainViewModel, city = city)
+            }
         }
 
         composable(WeatherScreens.SearchScreen.name) {
