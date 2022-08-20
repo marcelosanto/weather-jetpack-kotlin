9245382df65c30f6f6ff895f9eb396a63bc12dcf
Marcelo Santos <rocha.mer21@gmail.com>
2022-08-20 12:17:01 -0300

    feat: create db repository

---
+++ app/src/main/java/xyz/spacer/weatherforecast/repository/WeatherDbRepository.kt
@@ -0,0 +1,19 @@
+package xyz.spacer.weatherforecast.repository
+
+import kotlinx.coroutines.flow.Flow
+import xyz.spacer.weatherforecast.data.WeatherDao
+import xyz.spacer.weatherforecast.model.Favorite
+import javax.inject.Inject
+
+class WeatherDbRepository @Inject constructor(private val weatherDao: WeatherDao) {
+
+    fun getFavorites(): Flow<List<Favorite>> = weatherDao.getFavorites()
+
+    suspend fun insertFavorite(favorite: Favorite) = weatherDao.insertFavorite(favorite)
+
+    suspend fun updateFavorite(favorite: Favorite) = weatherDao.updateFavorite(favorite)
+
+    suspend fun deleteAllFavorites() = weatherDao.deleteAllFavorites()
+
+    suspend fun deleteFavorite(favorite: Favorite) = weatherDao.deleteFavorite(favorite)
+}
